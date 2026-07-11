c039a21068f0102c4ffe5156c1c30229cd5bda64
alex <129445432+mochamap1e@users.noreply.github.com>
2026-07-11 18:03:18 -0400

    Update [[path]].ts

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,8 +1,8 @@
 export function onRequest(context) {
     const url = new URL(context.request.url)
     
-    if (url.endsWith(".pages.dev")) {
-        return Response.redirect("https://mochamaple.cafe", 301)
+    if (url.hostname.endsWith(".pages.dev")) {
+        return Response.redirect("https://pointercrate2028.com", 301)
     }
     
     return context.next()
